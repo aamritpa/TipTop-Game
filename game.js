08dab2d871b0585c68ec0c9dcca7068b192c45fb
Amritpal Singh <aamritpa@sfu.ca>
2020-02-23 17:07:05 -0800

    front end look updated

--- game.js
+++ game.js
@@ -51,14 +51,13 @@ $(document).ready(setup)
 
 make= function(){
     
-    p= paper.path('M100,100 L100,800 M100,800 L800,800 M800,800 L800,100 M800,100 L100,100 Z')
     var category= $('#option').val()
     var i
     var shape = [];
     for (i=0;i<50;i++){
 
       if(category=='a'){
-        shape[i]=paper.rect(400, 400, 60, 60)
+        shape[i]=paper.rect(400, 400, 100, 100)
       }
 
 
@@ -74,22 +73,23 @@ make= function(){
     x = Math.floor(Math.random() * 700)-300 ;
     y = Math.floor(Math.random() * 700)-300 ;
 
+  
+
     shape_attr = {
    'transform': 't' + x + ',' + y,
    'fill': '#f00',
     }
-    shape[i].animate(shape_attr,setTimeout)
+    callback= function () { 
+      this.hide() 
     }
-    setTimeout(function(){
-      SVG.find("circle").remove();
-      SVG.find("rect").remove();
-      SVG.find("path").remove();
-    }, 2000);
+    shape[i].animate(shape_attr, 3000, 'linear',callback);
+    }
 }
 
 
 setup = function() {
   paper = Raphael('container',1000,1000)
+  p= paper.path('M0,0 L1000,0 M1000,0 L1000,1000 M1000,1000 L0,1000 M0,1000 L0,0 Z')// L800,800 M800,800 L800,100 M800,100 L0,0 Z')
   $('button').click(make)
 }
 $(document).ready(setup)
